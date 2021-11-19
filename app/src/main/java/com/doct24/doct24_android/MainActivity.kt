e7f9d4026241b97251a288e2f595685ce0c13d81
PrinceSch <sofy95@mail.ru>
2021-11-19 14:58:31 +0700

    главная активити, навигация

--- app/src/main/java/com/doct24/doct24_android/MainActivity.kt
+++ app/src/main/java/com/doct24/doct24_android/MainActivity.kt
@@ -2,10 +2,23 @@ package com.doct24.doct24_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.doct24.doct24_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView = binding.mainBottomBar
+        navController = Navigation.findNavController(this, R.id.container)
+
+        NavigationUI.setupWithNavController(navView, navController)
     }
 }
---
+++ app/src/main/java/com/doct24/doct24_android/view/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.doct24.doct24_android.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.doct24.doct24_android.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        navController = NavHostFragment.findNavController(this)
+        return binding.root
+    }
+}
