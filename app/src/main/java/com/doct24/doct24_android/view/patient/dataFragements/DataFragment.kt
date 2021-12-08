3f1977d2c5cdc2e4f9caab1afad5462045b70134
tc-service <technocopy@mail.ru>
2021-12-08 11:58:28 +0300

    Ripple effect для нажатий

--- app/src/main/java/com/doct24/doct24_android/view/patient/dataFragements/DataFragment.kt
+++ app/src/main/java/com/doct24/doct24_android/view/patient/dataFragements/DataFragment.kt
@@ -21,7 +21,7 @@ class DataFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         binding = FragmentDataBinding.inflate(inflater)
 
