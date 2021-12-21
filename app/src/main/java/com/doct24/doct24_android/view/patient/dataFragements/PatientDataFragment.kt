babafd1357937cac61ba92a5e6919e38a464d75e
tc-service <technocopy@mail.ru>
2021-12-21 13:34:11 +0300

    Страница данных пациента

--- app/src/main/java/com/doct24/doct24_android/view/patient/dataFragements/PatientDataFragment.kt
+++ app/src/main/java/com/doct24/doct24_android/view/patient/dataFragements/PatientDataFragment.kt
@@ -1,32 +1,38 @@
 package com.doct24.doct24_android.view.patient.dataFragements
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintSet.GONE
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import com.doct24.doct24_android.MainActivity
-import com.doct24.doct24_android.R
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.navigation.fragment.findNavController
+import com.doct24.doct24_android.databinding.FragmentPatientDataBinding
+
 
 class PatientDataFragment : Fragment() {
 
 //    private lateinit var viewModel: PatientDataViewModel
-
+    private var _binding: FragmentPatientDataBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentPatientDataBinding.inflate(inflater, container, false)
+        navController = NavHostFragment.findNavController(this)
 
+        return  binding.root
+    }
 
-        return inflater.inflate(R.layout.patient_data_fragment, container, false)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.patientDataBackButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 
 //    override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -34,5 +40,10 @@ class PatientDataFragment : Fragment() {
 //        viewModel = ViewModelProvider(this).get(PatientDataViewModel::class.java)
 //        // TODO: Use the ViewModel
 //    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
