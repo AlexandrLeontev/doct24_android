fe9ae67a038f6251c5bd80ac71950b6c31bff29a
Nikita-Vikulov <n.vikulov92@yandex.ru>
2022-01-27 02:26:30 +0300

    Добавил выбор пола в карточке пациента и добавил кнопки в профиле (рост, вес, группа крови).

--- app/src/main/java/com/doct24/doct24_android/view/patient/ProfileFragment.kt
+++ app/src/main/java/com/doct24/doct24_android/view/patient/ProfileFragment.kt
@@ -1,13 +1,15 @@
 package com.doct24.doct24_android.view.patient
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.NumberPicker
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -18,12 +20,12 @@ import com.doct24.doct24_android.R
 import com.doct24.doct24_android.databinding.FragmentProfileBinding
 import com.doct24.doct24_android.view.hide
 import com.doct24.doct24_android.view.show
+import kotlinx.android.synthetic.main.dialog_layout_custom.view.*
 import kotlinx.android.synthetic.main.fragment_home.materialButton
 import kotlinx.android.synthetic.main.fragment_home.popUpFutureRelease
 import kotlinx.android.synthetic.main.fragment_profile.*
 
-
-class ProfileFragment: Fragment() {
+class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
@@ -40,7 +42,16 @@ class ProfileFragment: Fragment() {
         navController = NavHostFragment.findNavController(this)
 
 
-        with(binding){
+        with(binding) {
+            cardHeght.setOnClickListener {
+                showDialogFragmentHeight()
+            }
+            cardWaight.setOnClickListener {
+                showDialogFragmentWeight()
+            }
+            cardBlood.setOnClickListener {
+                showDialogFragmentBlood()
+            }
             cardAllerg.setOnClickListener {
                 showPopUpNotify()
             }
@@ -79,10 +90,10 @@ class ProfileFragment: Fragment() {
 //                }
             }
 
-            profileViewModel.image.observe(this@ProfileFragment, {
+            profileViewModel.image.observe(viewLifecycleOwner) {
 //                binding.imPrfilePhoto.id = it
                 im_prfile_photo.setImageURI(it)
-            })
+            }
 
             profileViewModel.getImage()
 
@@ -90,6 +101,68 @@ class ProfileFragment: Fragment() {
 
         return binding.root
 
+    }
+
+    private fun showDialogFragmentHeight() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_layout_custom, null)
+        val dialogAlert = AlertDialog.Builder(requireActivity())
+            .setView(dialogView)
+            .show()
+        dialogAlert.window?.decorView?.setBackgroundResource(R.drawable.dialog_background)
+        val buttonAccept = dialogView.findViewById<ImageButton>(R.id.buttonAccept)
+        dialogView.textView.text = "Рост"
+        val numberPicker = dialogView.findViewById<NumberPicker>(R.id.numberPicker)
+        numberPicker.minValue = 50
+        numberPicker.maxValue = 250
+        numberPicker.wrapSelectorWheel = true
+        /* numberPicker.setOnValueChangedListener { picker, oldVal, newVal ->
+             val height = newVal
+         }*/
+        buttonAccept.setOnClickListener {
+            dialogAlert.dismiss()
+        }
+    }
+
+    private fun showDialogFragmentWeight() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_layout_custom, null)
+        val dialogAlert = AlertDialog.Builder(requireActivity())
+            .setView(dialogView)
+            .show()
+        dialogAlert.window?.decorView?.setBackgroundResource(R.drawable.dialog_background)
+        val buttonAccept = dialogView.findViewById<ImageButton>(R.id.buttonAccept)
+        dialogView.textView.text = "Вес"
+        val numberPicker = dialogView.findViewById<NumberPicker>(R.id.numberPicker)
+        numberPicker.minValue = 20
+        numberPicker.maxValue = 250
+        numberPicker.wrapSelectorWheel = true
+        /* numberPicker.setOnValueChangedListener { picker, oldVal, newVal ->
+             val height = newVal
+         }*/
+        buttonAccept.setOnClickListener {
+            dialogAlert.dismiss()
+        }
+    }
+
+    private fun showDialogFragmentBlood() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_layout_custom, null)
+        val dialogAlert = AlertDialog.Builder(requireActivity())
+            .setView(dialogView)
+            .show()
+        dialogAlert.window?.decorView?.setBackgroundResource(R.drawable.dialog_background)
+        val buttonAccept = dialogView.findViewById<ImageButton>(R.id.buttonAccept)
+        dialogView.textView.text = "Группа крови"
+        val numberPicker = dialogView.findViewById<NumberPicker>(R.id.numberPicker)
+        val values = arrayOf("O (I) Rh+", "O (I) Rh-", "A (II) Rh+", "А (II) Rh-", "B (III) Rh+", "B (III) Rh-", "AB (IV) Rh-")
+        numberPicker.minValue = 0
+        numberPicker.maxValue = values.size - 1
+        numberPicker.displayedValues = values
+        numberPicker.wrapSelectorWheel = true
+        /* numberPicker.setOnValueChangedListener { picker, oldVal, newVal ->
+             val height = newVal
+         }*/
+        buttonAccept.setOnClickListener {
+            dialogAlert.dismiss()
+        }
     }
 
     private fun pickImageFromGallery() {
@@ -100,20 +173,25 @@ class ProfileFragment: Fragment() {
 
     companion object {
         //image pick code
-        private val IMAGE_PICK_CODE = 1000;
+        private val IMAGE_PICK_CODE = 1000
+
         //Permission code
-        private val PERMISSION_CODE = 1001;
+        private val PERMISSION_CODE = 1001
     }
 
-     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when(requestCode){
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
             PERMISSION_CODE -> {
-                if (grantResults.size >0 && grantResults[0] ==
-                        PackageManager.PERMISSION_GRANTED){
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
                     //permission from popup granted
                     pickImageFromGallery()
-                }
-                else{
+                } else {
                     //permission from popup denied
                     Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
                 }
@@ -129,20 +207,20 @@ class ProfileFragment: Fragment() {
 //            profileViewModel.image.postValue(data?.)
         }
 
- }
-
-        private fun showPopUpNotify() {
-            popUpFutureRelease.show()
-            materialButton.setOnClickListener {
-                popUpFutureRelease.hide()
-            }
-
-        }
-
-        override fun onDestroy() {
-            super.onDestroy()
-            _binding = null
-
-        }
     }
+
+    private fun showPopUpNotify() {
+        popUpFutureRelease.show()
+        materialButton.setOnClickListener {
+            popUpFutureRelease.hide()
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+
+    }
+}
 
