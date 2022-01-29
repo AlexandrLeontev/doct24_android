fb3c3f9db0385312e8277064f5cce117a4905481
Alexandr <Almixleon@yandex.ru>
2022-01-29 13:06:58 +0400

    Начало работы с логикой переходов

--- app/src/main/java/com/doct24/doct24_android/view/authorization/DownloadActivity.kt
+++ app/src/main/java/com/doct24/doct24_android/view/authorization/DownloadActivity.kt
@@ -4,19 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.doct24.doct24_android.MainActivity
 import com.doct24.doct24_android.R
 
-class DownloadFragment : AppCompatActivity() {
+class DownloadActivity : AppCompatActivity() {
 
     var handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_download)
+        setContentView(R.layout.activity_download)
 
         handler.postDelayed({
-            startActivity(Intent(this@DownloadFragment, MainActivity::class.java))
+            startActivity(Intent(this@DownloadActivity, OnboardingFragmentOne::class.java))
             finish()
         }, 2000)
     }
--- app/src/main/java/com/doct24/doct24_android/view/authorization/OnboardingFragmentOne.kt
+++ app/src/main/java/com/doct24/doct24_android/view/authorization/OnboardingFragmentOne.kt
@@ -1,4 +1,44 @@
 package com.doct24.doct24_android.view.authorization
 
-class OnboardingFragmentOne {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.doct24.doct24_android.R
+import com.doct24.doct24_android.databinding.FragmentDoctorProfileBinding
+import com.doct24.doct24_android.databinding.FragmentOnboardingOneBinding
+import com.doct24.doct24_android.model.Doctor
+import com.doct24.doct24_android.view.doctor.DoctorProfileFragment
+
+class OnboardingFragmentOne : Fragment() {
+
+    private var _binding: FragmentOnboardingOneBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOnboardingOneBinding.inflate(inflater, container, false)
+        navController = NavHostFragment.findNavController(this)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.OnboardingFragmentOneButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
